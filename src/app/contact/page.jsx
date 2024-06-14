6260dfe63a7bbbd868c6fa5dcddcec390f3e53eb
Mohitjnn <jnmohit29@gmail.com>
2024-06-15 04:26:59 +0530

    initial commit

---
+++ src/app/contact/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <center>ContactPage</center>
+    </main>
+  );
+};
+
+export default Contact;
---
+++ src/app/projects/[id]/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Plotdetails = () => {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <center>Single Plot page</center>
+    </main>
+  );
+};
+
+export default Plotdetails;
