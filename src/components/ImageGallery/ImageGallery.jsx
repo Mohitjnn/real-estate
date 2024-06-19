9113d57a76a50be38bad5b6ef18724fbe835d14d
Mohitjnn <jnmohit29@gmail.com>
2024-06-20 03:21:11 +0530

    extra images sections added

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,41 +1,125 @@
 "use client";
 import React, { useState } from "react";
 import Image from "next/image";
+import Modal from "../cards/Modal";
+
+const data = [
+  {
+    text: "Description of image is here image1",
+    link: "/static/images/homePage1.png",
+  },
+  {
+    text: "Description of image is here image2",
+    link: "/static/images/homePage2.png",
+  },
+  {
+    text: "Description of image is here image3",
+    link: "/static/images/homePage3.png",
+  },
+  {
+    text: "Description of image is here image4",
+    link: "/static/images/homePage4.png",
+  },
+  {
+    text: "Description of image is here image5",
+    link: "/static/images/homePage5.png",
+  },
+  {
+    text: "Description of image is here image6",
+    link: "/static/images/homePage6.png",
+  },
+];
 
 const ImageGallery = () => {
-  const [selectedImage, setSelectedImage] = useState(
-    "/static/images/homePage1.png"
-  );
+  const n = 6;
+
+  const [clickedImg, setClickedImg] = useState(null);
+  const [currentIndex, setCurrentIndex] = useState(null);
+
+  const handleClick = (item, index) => {
+    setCurrentIndex(index);
+    setClickedImg(item.link);
+  };
+
+  const handelRotationRight = () => {
+    const totalLength = data.length;
+    if (currentIndex + 1 >= totalLength) {
+      setCurrentIndex(0);
+      const newUrl = data[0].link;
+      setClickedImg(newUrl);
+      return;
+    }
+    const newIndex = currentIndex + 1;
+    const newUrl = data.filter((item) => {
+      return data.indexOf(item) === newIndex;
+    });
+    const newItem = newUrl[0].link;
+    setClickedImg(newItem);
+    setCurrentIndex(newIndex);
+  };
+
+  const handelRotationLeft = () => {
+    const totalLength = data.length;
+    if (currentIndex === 0) {
+      setCurrentIndex(totalLength - 1);
+      const newUrl = data[totalLength - 1].link;
+      setClickedImg(newUrl);
+      return;
+    }
+    const newIndex = currentIndex - 1;
+    const newUrl = data.filter((item) => {
+      return data.indexOf(item) === newIndex;
+    });
+    const newItem = newUrl[0].link;
+    setClickedImg(newItem);
+    setCurrentIndex(newIndex);
+  };
 
   return (
     <div className="w-[50%] h-full flex flex-col space-y-2">
       <div className=" w-full h-[45vh] relative">
         <Image
-          src={selectedImage}
+          src={data[0].link}
           fill={true}
           priority={true}
-          alt="image1"
+          alt={data[0].text}
           className="object-cover rounded-t-xl"
         />
       </div>
       <div className="flex justify-between w-full h-fit">
-        {[...Array(5)].slice(1, 4).map((item, i) => (
-          <div
-            key={i}
-            className="w-[33%] h-[15vh] relative cursor-pointer"
-            onClick={() =>
-              setSelectedImage(`/static/images/homePage${i + 1}.png`)
-            }
-          >
+        {data.slice(1, 3).map((item, i) => (
+          <div key={i} className="w-[33%] h-[15vh] relative cursor-pointer">
             <Image
-              src={`/static/images/homePage${i + 1}.png`}
+              src={item.link}
               fill={true}
-              alt={`image${i + 1}`}
+              alt={item.text}
               className="object-cover"
             />
           </div>
         ))}
+        <div
+          className="w-[33%] h-[15vh] relative cursor-pointer z-0"
+          onClick={() => handleClick(data[3], 4)}
+        >
+          <Image
+            src={data[3].link}
+            fill={true}
+            alt={data[3].text}
+            className="object-cover"
+          />
+          <p className="text text-white absolute w-full h-full flex justify-center items-center bg-black/80">
+            {`+${data.length - 3} images`}
+          </p>
+        </div>
       </div>
+      {clickedImg && (
+        <Modal
+          clickedImg={clickedImg}
+          handelRotationRight={handelRotationRight}
+          setClickedImg={setClickedImg}
+          handelRotationLeft={handelRotationLeft}
+        />
+      )}
     </div>
   );
 };
---
+++ src/components/cards/Modal.jsx
@@ -0,0 +1,73 @@
+import React from "react";
+import Image from "next/image";
+const Modal = ({
+  clickedImg,
+  setClickedImg,
+  handelRotationRight,
+  handelRotationLeft,
+}) => {
+  const handleClick = (e) => {
+    if (e.target.classList.contains("dismiss")) {
+      setClickedImg(null);
+    }
+  };
+
+  return (
+    <>
+      <div
+        className="fixed -top-5 inset-0 flex items-center bg-black/80 dismiss z-30"
+        onClick={handleClick}
+      >
+        <Image
+          height={800}
+          width={800}
+          src={clickedImg}
+          alt="bigger pic"
+          className="block m-auto shadow-lg"
+        />
+        <span
+          className="absolute top-5 right-5 text-2xl text-white cursor-pointer dismiss"
+          onClick={handleClick}
+        >
+          X
+        </span>
+        <div
+          onClick={handelRotationLeft}
+          className="absolute left-0 top-1/2 w-12 h-12 flex bg-gray-700 justify-center items-center text-white cursor-pointer transform -translate-y-1/2"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="w-6 h-6"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path
+              fillRule="evenodd"
+              d="M9.707 16.707a1 1 0 01-1.414 0l-6-6a1 1 0 010-1.414l6-6a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l4.293 4.293a1 1 0 010 1.414z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </div>
+        <div
+          onClick={handelRotationRight}
+          className="absolute right-0 top-1/2 w-12 h-12 flex bg-gray-700 justify-center items-center text-white cursor-pointer transform -translate-y-1/2"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="w-6 h-6"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path
+              fillRule="evenodd"
+              d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Modal;
