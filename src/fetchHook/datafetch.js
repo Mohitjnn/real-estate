31d0c2c48ab9d3f0518ea91b9c02ba0ecddba1ce
Mohitjnn <jnmohit29@gmail.com>
2024-08-07 02:15:49 +0530

    production error 6

--- src/fetchHook/datafetch.js
+++ src/fetchHook/datafetch.js
@@ -1,20 +1,39 @@
-async function getData(propertyData) {
-  const response = await fetch(`${process.env.NEXT_PUBLIC_URL}/api`, {
-    // const response = await fetch(`http://192.168.29.147:3000/api`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(propertyData),
-  });
+// fetchHook/datafetch.js
 
-  return response.json();
+async function getData(propertyData) {
+  try {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_URL}/api`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(propertyData),
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    return await response.json();
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return null; // Return null or handle the error as needed
+  }
 }
 
 export default getData;
 
 export async function localData() {
-  const response = await fetch(`${process.env.NEXT_PUBLIC_URL}/data.json`);
-  // const response = await fetch(`http://192.168.29.147:3000/data.json`);
-  return response.json();
+  try {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_URL}/data.json`);
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    return await response.json();
+  } catch (error) {
+    console.error("Error fetching local data:", error);
+    return null; // Return null or handle the error as needed
+  }
 }
