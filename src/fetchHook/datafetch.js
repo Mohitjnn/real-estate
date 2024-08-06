8a07facd86b758bec62a74a6987844b2468db552
Mohitjnn <jnmohit29@gmail.com>
2024-08-07 01:44:58 +0530

    production check 2

--- src/fetchHook/datafetch.js
+++ src/fetchHook/datafetch.js
@@ -1,5 +1,6 @@
 async function getData(propertyData) {
-  const response = await fetch(`${process.env.NEXT_PUBLIC_URL}/api`, {
+  // const response = await fetch(`${process.env.NEXT_PUBLIC_URL}/api`, {
+  const response = await fetch(`http://192.168.29.147:3000/api`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -13,6 +14,7 @@ async function getData(propertyData) {
 export default getData;
 
 export async function localData() {
-  const response = await fetch(`${process.env.NEXT_PUBLIC_URL}/data.json`);
+  // const response = await fetch(`${process.env.NEXT_PUBLIC_URL}/data.json`);
+  const response = await fetch(`http://192.168.29.147:3000/data.json`);
   return response.json();
 }
