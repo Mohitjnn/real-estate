e8a6f649670a71e20689296fcaca0278fa25daf6
Mohitjnn <jnmohit29@gmail.com>
2024-08-07 01:49:17 +0530

    production build error

--- src/fetchHook/datafetch.js
+++ src/fetchHook/datafetch.js
@@ -1,6 +1,6 @@
 async function getData(propertyData) {
-  // const response = await fetch(`${process.env.NEXT_PUBLIC_URL}/api`, {
-  const response = await fetch(`http://192.168.29.147:3000/api`, {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_URL}/api`, {
+    // const response = await fetch(`http://192.168.29.147:3000/api`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -14,7 +14,7 @@ async function getData(propertyData) {
 export default getData;
 
 export async function localData() {
-  // const response = await fetch(`${process.env.NEXT_PUBLIC_URL}/data.json`);
-  const response = await fetch(`http://192.168.29.147:3000/data.json`);
+  const response = await fetch(`${process.env.NEXT_PUBLIC_URL}/data.json`);
+  // const response = await fetch(`http://192.168.29.147:3000/data.json`);
   return response.json();
 }
